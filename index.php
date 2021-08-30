aabc5522227c6d4541cb8c63cad17697cef3d492
OsmanBytyqi <obytyqi2001@gmail.com>
2021-08-30 13:34:05 +0200

    it's not finshed yet

--- index.php
+++ index.php
@@ -10,37 +10,85 @@
         <h1>New User Signup</h1>
         <form action="server.php" method="post">
 
-            <input type="text" name="username" placeholder="username">
+            <input type="text" name="username" placeholder="username" id="username">
             <p>Username must be  and contain 5 - 12 characters</p>
 
-            <input type="text" name="email" placeholder="email">
+            <input type="text" name="email" placeholder="email" id="email">
             <p>Email must be a valid address, e.g. myname.lastname@student.uni-pr.edu</p>
 
-            <input type="password" name="password" placeholder="password">
+            <input type="password" name="password" placeholder="password" id="password">
             <p>Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters</p>
 
-            <input type="text" name="telephone" placeholder="telephone">
+            <input type="text" name="telephone" placeholder="telephone" id="telephone">
             <p>Telephone must be a valid USA telephone number (11 digits)</p>
 
-            <input type="text" name="slug" placeholder="profile slug">
+            <input type="text" name="slug" placeholder="profile slug" id="slug">
             <p>Slug must contain only lowercase letters, numbers and hyphens and be 8 - 20 characters</p>
+            <input type="button" value="sign up" id="submit" name="submit">
 
-            <button type="submit" name="submit" id="submit">Sign up</button>
-
-          
+            <!-- <button type="submit" name="submit" id="submit">Sign up</button> -->
         </form>
+        <div class="correct" style="display:none">Your data has been save</div>
+            <div id="error"style="display:none" > something went worong :-)</div>
+          
         <script type="text/javascript" src="index.js"></script>
 
         <script>
 
-            // $(document).ready(function(){
-            //     $('form').on("submit",function(event){
+            $(document).ready(function(){
+                $('submit').click(function(event){
 
-            //         event.preventDefault();
+                var mydata{
+                  username:$("#username").val();
+                  email:$("#email").val();
+                  password:$("#password").val();
+                  telephone:$("#telephone").val();
+                  slug:$("#slug").val();
+                };
+                 $.ajax({
+                     url:"server.php",
+                     method:"POST",
+                     data:mydata,
+                       
+                
+                 }).done(function(data){
 
-            //     })
+                    if(data.error){
+                        $("#error").text(data.error).show();
+                    }
+                    else{
+                        $('.correct').text(data.name).show();
+                    }
 
-            // });
+
+                 })
+
+
+
+                  event.preventDefault();
+
+                })
+
+            });
+
+
         </script>
+
+        <style>
+            #error{
+                width:100%;
+                background-color:red;
+                height:25px;
+                text-align:center;
+            }
+
+            .correct{
+                height:25px;
+                width:100%;
+                background-color:green;
+                text-align:center;
+                
+            }
+            </style>
     </body>
 </html>
